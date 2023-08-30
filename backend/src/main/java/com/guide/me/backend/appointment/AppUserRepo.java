89949202f1eb839929dbd910249296c98cb89007
Muslim Kareem <muslimkareem@icloud.com>
2023-08-30 09:53:37 +0200

    Add @Repository annotation

--- backend/src/main/java/com/guide/me/backend/appointment/AppUserRepo.java
+++ backend/src/main/java/com/guide/me/backend/appointment/AppUserRepo.java
@@ -1,6 +1,8 @@
 package com.guide.me.backend.appointment;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepo extends MongoRepository<String,AppUser> {
 }
