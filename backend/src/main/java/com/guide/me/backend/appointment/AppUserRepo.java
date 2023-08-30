9b8257345dbdeb23af5da32990f8d7772cfa6e59
Muslim Kareem <muslimkareem@icloud.com>
2023-08-30 10:52:03 +0200

    Fix mongodb id position

--- backend/src/main/java/com/guide/me/backend/appointment/AppUserRepo.java
+++ backend/src/main/java/com/guide/me/backend/appointment/AppUserRepo.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppUserRepo extends MongoRepository<String,AppUser> {
+public interface AppUserRepo extends MongoRepository<AppUser,String> {
 }
