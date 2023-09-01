c729966635f63a2d7c2b9c568323e9270df58a25
Muslim Kareem <muslimkareem@icloud.com>
2023-09-01 09:55:39 +0200

    Create SecurityConfig

---
+++ backend/src/main/java/com/guide/me/backend/securityconfig/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.guide.me.backend.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests(c -> {
+                    c.requestMatchers(HttpMethod.POST, "/api/place-holder").authenticated();
+//                    c.requestMatchers(HttpMethod.PUT, "/api/place-holder/**").authenticated();
+//                    c.requestMatchers(HttpMethod.DELETE, "/api/place-holder/**").hasRole(Role.ADMIN.toString());
+//                    c.anyRequest().permitAll();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(Customizer.withDefaults())
+                .build();
+    }
+
+}
