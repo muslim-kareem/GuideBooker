346700a9af9aed96c61d4506587f159a89fa8a5c
Muslim Kareem <muslimkareem@icloud.com>
2023-09-01 09:52:19 +0200

    Create findAppUserByName method

--- backend/src/main/java/com/guide/me/backend/appointment/AppUserRepo.java
+++ backend/src/main/java/com/guide/me/backend/appointment/AppUserRepo.java
@@ -3,6 +3,9 @@ package com.guide.me.backend.appointment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepo extends MongoRepository<AppUser,String> {
+    Optional<AppUser> findAppUserByName(String username);
 }
